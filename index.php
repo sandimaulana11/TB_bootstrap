87246f8529197699f9ec3d08040d3673eac15547
Indra <mf180053@gmail.com>
2021-02-26 14:59:11 +0700

    progress 1

---
+++ index.php
@@ -0,0 +1,200 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+            <!-- Bootstrap CSS -->
+           <!--  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+            <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+
+            <title>Hello, world!</title>
+    </head>
+    <style type="text/css">
+        
+    </style>
+    <body>
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
+            <a class="navbar-brand" href="#">ALL ABOUT BURGER</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Bahan-Bahan">Bahan-Bahan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#gallery">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact Us</a>
+                    </li>
+                </ul>                
+            </div>
+        </nav>
+
+            <section id="home">
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/b1.jpg" class="d-block w-100" alt="..." style="height: 720px;">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/b2.jpg" class="d-block w-100" alt="..." style="height: 720px;">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="height: 720px;">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+            </section>
+            <div style="background-image: bg3.jpg">
+            <section id="Bahan-Bahan">
+
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-8 mx-auto text-center">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+              
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>
+                    <div class="card">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    </th>
+                    <th>
+                    <div class="card">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    </th>
+                    <th>
+                    <div class="card">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    </th>
+                    <th>
+                    <div class="card">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    </th>
+                    <th>
+                    <div class="card">
+                        <img src="img/b3.jpg" class="d-block w-100" alt="..." style="">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                    </th>
+                </tr>
+              </table>
+              </div>
+          
+           <section id="gallery">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 mx-auto text-center">
+                            <h1> ini adalah bagian gallery</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="contact">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/b4.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption col-ml-11 mx-1 text-left">
+                        <div calss="container">
+                            <div class="row">
+                                <div class="col-md-12 text-center" style="margin-bottom: 70%;">
+                                     <h1> ini adalah bagian Contact</h1>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum nisi illum cumque repellat, deserunt delectus in necessitatibus quas blanditiis dolorem a quis quos quaerat fugit. Officiis ex corporis ut beatae.</p>
+                                </div>                       
+                            </div>
+                        </div>
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </section>
+
+            <!--footer-->
+            <footer class="bg-light text-center text-lg-start">
+                <div class="text-center p-3" style="background-color: white;">
+                    <h5>All About Burger©2021 Copyright</h5>
+                <a class="text-dark" href="..../"></a>
+                </div>
+            </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+ 
+    </body>
+</html>
