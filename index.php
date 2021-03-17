73cbc28f5e89458a2f139cb82071c69e282343d6
indrafathur <indrafathur07@gmail.com>
2021-03-17 14:15:14 +0700

    merapihkan

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         }
     </style>
     <body style="background-color: #7f1422">
-    <button onclick="topFunction()" id="myBtn" title="Go to top">HOME</button>
+    <button onclick="topFunction()" id="myBtn" title="Go to top">.HOME.</button>
         <nav class="navbar navbar-expand-lg fixed-top navbar-dark" style="background-color: #7f1422">
             <a class="navbar-brand" href="#">ALL ABOUT BURGER</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
